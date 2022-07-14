7489039f05d9c14574d6e49fac2c02d16b8e6656
Hyuyeong <choibeny@gmail.com>
2022-07-14 21:45:56 +1200

    Dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 // import DetailComment from './components/DetailComment';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 function App() {
+  const options = [
+    {
+      label: 'The Color Red',
+      value: 'red',
+    },
+    {
+      label: 'The Color Green',
+      value: 'green',
+    },
+    {
+      label: 'The Color Blue',
+      value: 'blue',
+    },
+  ];
+
   return (
     <div className="ui container comments">
       {/* <DetailComment author={'sam'} date={'6/28'} text={'good !!!'} />
       <DetailComment />
       <DetailComment /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 }
